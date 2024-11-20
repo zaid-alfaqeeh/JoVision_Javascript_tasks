b71bc8866e2626a2670ffc249cfa814358484f3b
zaid alfaqeeh <zaidalfaqeh111@gmail.com>
2024-11-20 13:48:37 +0300

    rename folder

--- task9/script.js
+++ task9/script.js
@@ -1,10 +1,6 @@
 function getinfo() {
-    fetch("https://api.ipify.org?format=json")
-      .then((response) => response.json()) 
-      .then((data) => console.log(data)) 
-      .catch((error) => console.error("Error:", error)); 
-<<<<<<< HEAD:task9/script.js
-  }
-=======
-  }
->>>>>>> a35aa5f5fc67b51890c2959c131da8839556c79b:task6/script.js
+  fetch("https://api.ipify.org?format=json")
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error:", error));
+}
